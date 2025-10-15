b7804261419444e095d16aaf283c6e02c2bb3634
Valentin Petrovych <72038591+ValentinPostindustria@users.noreply.github.com>
2025-10-15 07:57:34 +0200

    Fix unit tests (#24)

--- android_sdk/src/test/java/co/optable/android_sdk/OptableSDKUnitTest.kt
+++ android_sdk/src/test/java/co/optable/android_sdk/OptableSDKUnitTest.kt
@@ -8,13 +8,15 @@ import android.net.Uri
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotEquals
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
 
 /**
  * OptableSDK unit tests
  */
+@RunWith(RobolectricTestRunner::class)
 class OptableSDKUnitTest {
 
-    // TODO: Fix
     @Test
     fun eid_isCorrect() {
         val expected = "e:a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3"
